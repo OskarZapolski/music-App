8f12e71117573de45927c6805da0d489ad89eb18
Oskar Zapolski <oskar.zapolski9@gmail.com>
2025-06-23 09:17:49 +0200

    I have added animations to log in

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,14 +2,29 @@ export default function LogIn() {
   const AUTH_URL =
     "https://accounts.spotify.com/authorize?client_id=aa11595a5869411eacc30f6af0af738d&response_type=token&redirect_uri=https://oskarzapolski.github.io/music-App/&scope=streaming%20user-read-email%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-private%20user-read-currently-playing";
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-black text-white">
-      <div className="grid grid-cols-1 gap-10 items-center place-items-center">
+    <div className="w-screen h-screen flex justify-center items-center flex-col bg-black text-white ">
+      <div className="z-10 w-[23%] relative isolate">
+        <div className="absolute rounded-full bg-yellow-700 mix-blend-multiply blur-xl  size-44 2xl:size-72 -top-6 animate-pulse-subtle z-10 animate-blob absolute -left-[3.5%]"></div>
+        <div className="absolute rounded-full bg-purple-700 mix-blend-multiply blur-xl  -right-[3.5%] size-44 2xl:size-72 -top-6 animate-pulse-subtle z-10 animate-blob3 absolute "></div>
+        <div className="absolute rounded-full bg-pink-700 mix-blend-multiply blur-xl top-12 size-44 2xl:size-72 left-[5%] 2xl:left-[14.5%]  animate-pulse-subtle z-10 animate-blob6 absolute "></div>
+      </div>
+      <div className="absolute right-5 top-5 w-10 h-1 shadow-xl shadow-white rotate-45 z-20"></div>
+      <div className="grid grid-cols-1 gap-10 items-center place-items-center z-20">
         <h1 className="text-white text-3xl">
-          This is a personal project using the Spotify API. Not affiliated with
-          Spotify
+          <span>
+            <span className="animate-fadein opacity-0">This is a </span>
+            <span className="animate-fadein4 opacity-0">personal project </span>
+            <span className="animate-fadein6 opacity-0">
+              using the Spotify{" "}
+            </span>
+            <span className="animate-fadein8 opacity-0"> API. Not </span>
+            <span className="animate-fadein10 opacity-0">
+              affiliated with Spotify{" "}
+            </span>
+          </span>
         </h1>
         <a href={AUTH_URL}>
-          <button className="sm:p-4 p-3 bg-purple-600 rounded-3xl cursor-pointer  text-xl active:translate-y-1 hover:bg-purple-800 transition duration-150">
+          <button className="sm:p-4 p-3 bg-purple-500 rounded-3xl cursor-pointer  text-xl active:translate-y-1 hover:bg-purple-800 transition duration-150 animate-fadein12 opacity-0">
             Log through spotify premium account
           </button>
         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 8s ease-in-out infinite",
+        blob3: "blob 8s ease-in-out 3s infinite",
+        blob6: "blob 8s ease-in-out 6s infinite",
+        fadein: "fadein 0.7s ease-out 0.2s forwards",
+        fadein4: "fadein 0.7s ease-out 0.4s forwards",
+        fadein6: "fadein 0.7s ease-out 0.6s forwards",
+        fadein8: "fadein 0.7s ease-out 0.8s forwards",
+        fadein10: "fadein 0.7s ease-out 1s forwards",
+        fadein12: "fadein 0.7s ease-out 1.2s forwards",
+      },
+      keyframes: {
+        blob: {
+          "0% 100%": {
+            transform: "translate(0px,0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(20px,-35px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-10px, 20px) scale(0.9)",
+          },
+        },
+        fadein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(20px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+    },
   },
   plugins: [
     require("tailwind-scrollbar"),
